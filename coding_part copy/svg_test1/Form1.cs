d0d33ec77178f9031bb2493e8b09d9f6b49c08a5
Artem <125925516+NuroO1@users.noreply.github.com>
2023-02-26 19:09:18 +0300

    renaming

---
+++ coding_part copy/svg_test1/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace svg_test1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) //открытие окна с прочим
+        {
+            Form1 frm1 = new Form1();
+            Form2 frm2 = new Form2();
+            frm2.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = ""; //обнуление статуса файла
+
+            string userName1 = Environment.UserName; //получение имя пользователя
+
+            string[] path = new string[6]; //создание массива путей к нужным файлам
+
+            for (int p = 0; p < 6; p++)  //задаём значение этому массиву
+            {
+                path[p] = $"C:\\Users\\{userName1}\\Downloads\\Layer{p + 1}.gcode";
+            }
+
+            string[][] lines = new string[6][]; //создание массива массивов строк
+
+            for (int l = 0; l < 6; l++)
+            {
+                try
+                {
+                    lines[l] = File.ReadAllLines(path[l]); //записываем строки в массив массивов
+                }
+                catch
+                {
+                    break;
+                }
+            }
+
+            if (Directory.Exists("C:\\Чертёжник") == false) //проверка наличии папки. если нет, то создает
+            {
+                Directory.CreateDirectory("C:\\Чертёжник");
+            }
+
+            int i = 1;
+            string fileName = $"C:\\Чертёжник\\Draft_{i}"; //задаём путь к файлу
+
+            try
+            {
+                while (File.Exists(fileName) == true)  //проверка наличия файла с данным названием. перебираются названия до тех пор, пока не найдется свободного
+                {
+                    i++;
+                    fileName = $"C:\\Чертёжник\\Draft_{i}";
+                }
+
+                using (FileStream f = File.Create(fileName))  //создание файла, в который запишем данные из скачанных файлов 
+                {
+                    byte[] info = new UTF8Encoding(true).GetBytes("");
+                    f.Write(info, 0, info.Length);
+                }
+            }
+            catch (Exception Ex)  //проверка на ошибку
+            {
+                Console.WriteLine(Ex.ToString());
+            }
+            using (StreamWriter file = new StreamWriter(fileName, append: true)) //запись содержимого ранних файлов в файл, который мы создали
+            {
+                for (int a = 0; a < 6; a++)
+                {
+                    try
+                    {
+                        for (int b = 0; b < lines[a].Length; b++)
+                        {
+                            file.WriteLine(lines[a][b]);
+                        }
+                    }
+                    catch
+                    {
+                        break;
+                    }
+                }
+            }
+            for (int n = 0; n < 6; n++)
+            {
+                try
+                {
+                    File.Delete(path[n]); //удаляем ранее использованные ненужные файлы
+                }
+                catch
+                {
+                    break;
+                }
+            }
+            File.Delete($"C:\\Чертёжник\\Draft_{i}.gcode");
+            File.Move($"C:\\Чертёжник\\Draft_{i}", $"C:\\Чертёжник\\Draft_{i}.gcode");
+            if (File.Exists($"C:\\Чертёжник\\Draft_{i}.gcode") == true)
+            {
+                textBox1.Text = "Файл создан";
+            } //статус файла
+        }
+
+        private void button3_Click(object sender, EventArgs e) //открытие папки с чертежами
+        {
+            Process.Start("C:\\Чертёжник");
+        }
+
+        private void button4_Click(object sender, EventArgs e) //открытие папки Загрузки
+        {
+            string userName2 = Environment.UserName;
+            Process.Start($"C:\\Users\\{userName2}\\Downloads");
+        }
+
+        private void button5_Click(object sender, EventArgs e) //открытие окна с инструкцией
+        {
+            Form1 frm1 = new Form1();
+            Form3 frm3 = new Form3();
+            frm3.Show();
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (webView != null && webView.CoreWebView2 != null)
+            {
+                webView.CoreWebView2.Navigate("https://sameer.github.io/svg2gcode/#close");
+            }
+        }
+    }
+}
---
+++ coding_part copy/svg_test1/Form3.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace svg_test1
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(18, 20);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1263, 160);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(1292, 449);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(316, 165);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "Называть svg файлы слоёв чертежа необходимо, как Layer1, Layer2 и т. д., зависимо" +
+    " от номера слоя.\r\nМаксимум слоёв - 6.";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::svg_test1.Properties.Resources.photo2023;
+            this.pictureBox1.Location = new System.Drawing.Point(18, 189);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1263, 732);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(1626, 940);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form3";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Чертёжник (Инструкция)";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
